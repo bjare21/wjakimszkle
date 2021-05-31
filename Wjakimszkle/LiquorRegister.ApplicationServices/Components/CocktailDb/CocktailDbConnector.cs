e2522726d74806ad0643fab9192c49ca2b57c43e
bjare21 <83592746+bjare21@users.noreply.github.com>
2021-05-31 16:39:31 +0200

    Update CocktailDbConnector.cs

--- Wjakimszkle/LiquorRegister.ApplicationServices/Components/CocktailDb/CocktailDbConnector.cs
+++ Wjakimszkle/LiquorRegister.ApplicationServices/Components/CocktailDb/CocktailDbConnector.cs
@@ -22,7 +22,7 @@ namespace Wjakimszkle.ApplicationServices.Components.CocktailDb
         {
             var request = new RestRequest($"search.php?i={ingredientName}",Method.GET);
             
-            request.AddHeader("x-rapidapi-key", "22d8e5afd0msh1583b8d43be4940p17d966jsn175d20e5b4b9");
+            request.AddHeader("x-rapidapi-key", "{api-key}");
             request.AddHeader("x-rapidapi-host", "the-cocktail-db.p.rapidapi.com");
             var queryResult = await restClient.ExecuteAsync(request);
             var cocktails = JsonConvert.DeserializeObject<Cocktail>(queryResult.Content);
